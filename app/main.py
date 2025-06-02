e1747da3a29cbaf0b52068b9e3d2d584a777d7d6
henadz375 <henadz375@gmail.com>
2025-06-02 09:10:11 +0200

    Created initial view with welcome message

---
+++ app/main.py
@@ -0,0 +1,17 @@
+import uvicorn
+from fastapi import FastAPI
+from app.routers import categories, products
+
+app = FastAPI()
+
+app.include_router(categories.router)
+app.include_router(products.router)
+
+
+@app.get('/')
+async def welcome():
+    return {'message': 'welcome'}
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
